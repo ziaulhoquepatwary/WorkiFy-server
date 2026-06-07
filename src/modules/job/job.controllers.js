fd8b5bd9539d0e8efb261a317049b3ec7d910bee
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-06-07 23:09:51 +0600

    Create catchAsync function for handle tryCatch system

--- src/modules/job/job.controllers.js
+++ src/modules/job/job.controllers.js
@@ -1,40 +1,32 @@
 import AppError from "../../utils/AppError.js";
+import catchAsync from "../../utils/catchAsync.js";
 import Job from "./job.model.js";
 import { jobValidationSchema } from "./job.validation.js";
 
-export const createJob = async (req, res) => {
-    try {
-        const body = req.body;
+export const createJob = catchAsync(async (req, res) => {
+    const body = req.body;
 
-        // console.log("---------------- BACKEND CHECK START ----------------");
-        // console.log("GET JOB Data:", req.body);
-        // console.log("GET Cookies:", req.cookies);
-        // console.log("----------------- BACKEND CHECK END -----------------");
+    // console.log("---------------- BACKEND CHECK START ----------------");
+    // console.log("GET JOB Data:", req.body);
+    // console.log("GET Cookies:", req.cookies);
+    // console.log("----------------- BACKEND CHECK END -----------------");
 
+    const parsed = jobValidationSchema.safeParse(body);
 
-        const parsed = jobValidationSchema.safeParse(body);
-
-        if (!parsed.success) {
-            throw new AppError(400, "Validation failed")
-        }
-
-        const newJob = await Job.create({
-            ...parsed.data,
-            author_id: req.user.id,
-            author_name: req.user.name,
-            author_email: req.user.email,
-        });
-
-        res.status(201).json({
-            success: true,
-            message: "Job Create Successfully",
-            job: newJob
-        })
-
-    } catch (error) {
-        res.status(500).json({
-            success: false,
-            message: error.message,
-        });
+    if (!parsed.success) {
+        throw new AppError(400, "Validation failed")
     }
-}
+
+    const newJob = await Job.create({
+        ...parsed.data,
+        author_id: req.user.id,
+        author_name: req.user.name,
+        author_email: req.user.email,
+    });
+
+    res.status(201).json({
+        success: true,
+        message: "Job Create Successfully",
+        job: newJob
+    })
+})
