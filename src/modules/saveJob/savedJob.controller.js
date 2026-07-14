3ca22befcada77e0b2b5dc49ea56f9af56ff6238
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-07-14 22:11:08 +0600

    Create toggleSaveJob API controller

---
+++ src/modules/saveJob/savedJob.controller.js
@@ -0,0 +1,35 @@
+import catchAsync from "../../utils/catchAsync.js";
+import SavedJob from "./saveJob.model.js";
+
+
+export const toggleSaveJob = catchAsync(async (req, res) => {
+    const userId = req.user.id;
+    const { jobId } = req.params;
+
+    if (!jobId) {
+        return res.status(400).json({
+            success: false,
+            message: "Job ID is required.",
+        });
+    }
+
+    const existingSave = await SavedJob.findOne({ userId, jobId });
+
+    if (existingSave) {
+        await SavedJob.findByIdAndDelete(existingSave._id);
+
+        return res.status(200).json({
+            success: true,
+            isSaved: false,
+            message: "Job removed from saved list.",
+        });
+    }
+
+    await SavedJob.create({ userId, jobId });
+
+    res.status(201).json({
+        success: true,
+        isSaved: true,
+        message: "Job saved successfully.",
+    });
+});
